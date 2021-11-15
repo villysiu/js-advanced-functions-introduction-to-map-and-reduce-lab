7b78628537f12b610eb702ac42bbfc5b9abcee50
villysiu <villysiu@gmail.com>
2021-11-14 21:10:37 -0800

    commit tree 5bb9f364d8d4c856cf375ba16388bf9a0b995332

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+let mapToNegativize = (sourceArray) => {
+    return sourceArray.map(x => x * -1)
+}
+let mapToNoChange= (sourceArray) => {
+    return sourceArray.map(x => x)
+}
+let mapToDouble = (sourceArray) => sourceArray.map(x=>x*2)
+
+let mapToSquare = (sourceArray) => sourceArray.map(x => x * x)
+
+let reduceToTotal = (sourceArray, startingPoint=0) => {
+    return sourceArray.reduce(((prev, curr) => prev+curr), startingPoint)
+}
+let reduceToAllTrue = (sourceArray) =>{
+    return sourceArray.reduce((p,c ) => !!c && !!p)
+}
+let reduceToAnyTrue = sourceArray => {
+    return sourceArray.reduce((p,c) => !!c || !!p)
+}
